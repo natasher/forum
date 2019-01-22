cc0f83d34a510a30b3eafde472ab0ec97cd5f236
Artur Woźniak <artur.wozniak69@gmail.com>
2019-01-22 12:04:03 +0100

    Create feature/ThreadTest file.
    By renaming ExampleTest. Added one TestCase.

---
+++ tests/Feature/ThreadTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ThreadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+}
