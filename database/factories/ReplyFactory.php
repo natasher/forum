d391a1f0c7c18a6cda69cafb0721eeb5f68d9e07
Artur Woźniak <artur.wozniak69@gmail.com>
2019-01-22 11:22:53 +0100

    Filled ReplyFactory.

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -4,6 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Reply::class, function (Faker $faker) {
     return [
-        //
+        'thread_id' => function () {
+            return factory( App\Thread::class )->create()->id;
+        },
+        'user_id' => function () {
+            return factory( App\User::class )->create()->id;
+        },
+        'body' => $faker->paragraph,
     ];
 });
