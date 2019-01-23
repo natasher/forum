730736b8cbaf53f27f0deaddd8479bff01a84f01
Artur Woźniak <artur.wozniak69@gmail.com>
2019-01-23 13:43:49 +0100

    RED: Added TC a_thread_has_a_creator

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use App\Thread;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -10,12 +11,22 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ThreadTest extends TestCase
 {
 
+    use RefreshDatabase;
+
     /** @test */
     function a_thread_has_replies()
     {
         $thread = factory( Thread::class )->create();
 
         $this->assertInstanceOf( 'Illuminate\Database\Eloquent\Collection', $thread->replies );
+    }
+
+    /** @test */
+    function a_thread_has_a_creator()
+    {
+        $thread = factory( Thread::class )->create();
+
+        $this->assertInstanceOf( User::class, $thread->creator );
     }
 
 }
