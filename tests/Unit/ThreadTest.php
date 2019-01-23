ce7bc151875125354e0f6146de4eb74cdcd04dfe
Artur Woźniak <artur.wozniak69@gmail.com>
2019-01-23 13:32:53 +0100

    Added new test file ThreadTest <unit>

---
+++ tests/Unit/ThreadTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Thread;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ThreadTest extends TestCase
+{
+}
