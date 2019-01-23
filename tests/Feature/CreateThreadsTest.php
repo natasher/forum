0f3e8e692b8859a7b5b5c7546ec177bbdfda4f44
Artur Woźniak <artur.wozniak69@gmail.com>
2019-01-23 16:39:51 +0100

    Added CreateThreadsTest TS

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateThreadsTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+
+}
