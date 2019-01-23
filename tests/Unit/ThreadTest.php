48a3f801b845af657e276d6617fa857bdba065f1
Artur Woźniak <artur.wozniak69@gmail.com>
2019-01-23 13:33:19 +0100

    GREEN: Added TC a_thread_has_replies

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -9,4 +9,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ThreadTest extends TestCase
 {
+
+    /** @test */
+    function a_thread_has_replies()
+    {
+        $thread = factory( Thread::class )->create();
+
+        $this->assertInstanceOf( 'Illuminate\Database\Eloquent\Collection', $thread->replies );
+    }
+
 }
