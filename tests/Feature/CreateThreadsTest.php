9c5c473455308856fdd631ee15480fca92364233
Artur Woźniak <artur.wozniak69@gmail.com>
2019-01-24 10:06:26 +0100

    RED: Added TC, user can create new thread.

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use App\Thread;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,5 +13,18 @@ class CreateThreadsTest extends TestCase
 
     use RefreshDatabase;
 
+    /** @test */
+    function an_authenticated_user_can_create_new_forum_thread()
+    {
+        $this->actingAs( factory( User::class )->create() );
+
+        $thread = factory( Thread::class )->make();
+
+        $this->post( '/threads', $thread->toArray() );
+
+        $this->get( $thread->path() )
+            ->assertSee( $thread->title )
+            ->assertSee( $thread->body );
+    }
 
 }
