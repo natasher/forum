d1d16f361e5dc96afc681cb83c6cee5724bb0197
Artur Woźniak <artur.wozniak69@gmail.com>
2019-01-24 15:30:29 +0100

    ADDED form to threads.create
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -5,10 +5,27 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"></div>
+                <div class="card-header">Create a New Thread</div>
 
                 <div class="card-body">
+                    <form action="/threads" method="post">
+                        @csrf
+
+                        <div class="form-group">
+                            <label for="title">Title:</label>
+                            <input type="text" name="title" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="body">Body:</label>
+                            <textarea name="body" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Publish</button>
+
+                    </form>
                 </div>
+
             </div>
         </div>
     </div>
