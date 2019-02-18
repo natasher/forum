d3b1cec1fb39f0bc501e1eaa14f284d909ce303f
Artur Woźniak <artur.wozniak69@gmail.com>
2019-02-18 14:02:13 +0100

    Added channel_id validation rules
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -44,8 +44,9 @@ class ThreadsController extends Controller
     public function store(Request $request)
     {
         $this->validate( $request, [
-            'title' => 'required',
-            'body'  => 'required',
+            'title'      => 'required',
+            'body'       => 'required',
+            'channel_id' => 'required|exists:channels,id',
         ]);
 
         $thread = Thread::create([
