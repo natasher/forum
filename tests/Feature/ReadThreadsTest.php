c1272b467964269897286a44454f502798c26539
Artur Woźniak <artur.wozniak69@gmail.com>
2019-02-18 16:23:05 +0100

    Fixed typo
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -42,7 +42,7 @@ class ThreadTest extends TestCase
     }
 
     /** @test */
-    function a_user_can_filter_threads_according_to_a_tag()
+    function a_user_can_filter_threads_according_to_a_channel()
     {
         $channel = create( 'App\Channel' );
         $threadInChannel = create( 'App\Thread', [ 'channel_id' => $channel->id ]);
