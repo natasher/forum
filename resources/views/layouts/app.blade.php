0eb6d25451878fb4d5e94b1d149874978fc3055c
Artur Woźniak <artur.wozniak69@gmail.com>
2019-02-19 10:39:23 +0100

    Added nav link to create Thread
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/threads">All Threads</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/threads/create">New Thread</a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Channels
@@ -45,8 +48,7 @@
                                     <a class="dropdown-item" href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a>
                                 @endforeach
                             </div>
-                        </div>
-                    </li>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
