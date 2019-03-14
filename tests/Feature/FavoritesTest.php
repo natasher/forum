5a80a238e14835502a66c66850edaa30d7ed2641
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-14 14:29:41 +0100

    Added TC: an_authenticated_user_may_only_favorite_a_reply_once
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- tests/Feature/FavoritesTest.php
+++ tests/Feature/FavoritesTest.php
@@ -33,4 +33,17 @@ class FavoritesTest extends TestCase
         $this->assertCount( 1, $reply->favorites );
     }
 
+    /** @test */
+    public function an_authenticated_user_may_only_favorite_a_reply_once()
+    {
+        $this->signIn();
+
+        $reply = create( 'App\Reply' );
+
+        $this->post( 'replies/' . $reply->id . '/favorites' );
+        $this->post( 'replies/' . $reply->id . '/favorites' );
+
+        $this->assertCount( 1, $reply->favorites );
+    }
+
 }
