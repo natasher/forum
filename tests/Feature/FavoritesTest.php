70e819f899fddcbccb5ae81251fa5844437db65f
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-14 14:34:01 +0100

    Tailor error message - try / catch block
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- tests/Feature/FavoritesTest.php
+++ tests/Feature/FavoritesTest.php
@@ -40,8 +40,12 @@ class FavoritesTest extends TestCase
 
         $reply = create( 'App\Reply' );
 
-        $this->post( 'replies/' . $reply->id . '/favorites' );
-        $this->post( 'replies/' . $reply->id . '/favorites' );
+        try {
+            $this->post( 'replies/' . $reply->id . '/favorites' );
+            $this->post( 'replies/' . $reply->id . '/favorites' );
+        } catch ( \Exception $e ) {
+            $this->fail( 'Did not expect to insert the same record set twice.' );
+        }
 
         $this->assertCount( 1, $reply->favorites );
     }
