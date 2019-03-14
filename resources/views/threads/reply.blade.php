ed1d3b144de7652edd26877d2b908f5a5435b747
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-14 15:43:45 +0100

    Added form to reply
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>
    
    more refactor
    
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,6 +1,19 @@
 <div class="card mt-2">
     <div class="card-header">
-        {{ $reply->owner->name }} said {{ $reply->created_at->diffForHumans() }}...
+        <div class="level">
+            <h5 class="flex">
+                <a href="#">
+                    {{ $reply->owner->name }}
+                </a> said {{ $reply->created_at->diffForHumans() }}...
+            </h5>
+
+            <div>
+                <form action="/replies/{{ $reply->id }}/favorites" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-primary">Favorite</button>
+                </form>
+            </div>
+        </div>
     </div>
 
     <div class="card-body">
