d446691dc6141def2553e6f55501d6ef1ba28a90
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-14 11:34:00 +0100

    Added popular filter
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>
    
    fix
    
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -8,16 +8,33 @@ class ThreadFilters extends Filters
 {
 
     /**
+     * Registered filters to operate upon.
+     *
+     * @var array
+     */
+    protected $filters = [ 'by', 'popular' ];
+
+    /**
      * Filter the query by a given username.
      *
      * @param string $username
-     * @return mixed
+     * @return Builder
      */
     protected function by( $username )
     {
         $user = User::where( 'name', $username )->firstOrFail();
 
         return $this->builder->where( 'user_id', $user->id );
+    }
+
+    /**
+     * Filter the query according to most popular threads
+     *
+     * @return Builder
+     */
+    protected function popular()
+    {
+        return $this->builder->orderBy( 'replies_count', 'desc' );
     }
 
 }
