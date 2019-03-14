e990348024edad7f689bfe29a260b7a8c4bbda8d
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-14 12:29:28 +0100

    Added TC an_authenticated_user_can_favorite_any_reply
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>
    
    create Reply
    
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- tests/Feature/FavoritesTest.php
+++ tests/Feature/FavoritesTest.php
@@ -12,5 +12,16 @@ class FavoritesTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
+    public function an_authenticated_user_can_favorite_any_reply()
+    {
+        // /replies/id/favorites
+        $reply = create( 'App\Reply' );
+
+        // If I post to a "favorite" endpoint
+        $this->post( 'replies/' . $reply->id . '/favorites' );
+
+        // It should be recorded in the database.
+        $this->assertCount( 1, $reply->favorites );
+    }
 
 }
