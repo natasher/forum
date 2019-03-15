3ca02ad04eaf6d6705b056f9272e1ecc07ebde41
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-15 15:12:33 +0100

    Refactor isFavorited()
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Reply.php
+++ app/Reply.php
@@ -33,7 +33,7 @@ class Reply extends Model
 
     public function isFavorited()
     {
-        return $this->favorites->where( 'user_id', auth()->id() )->exists();
+        return !!$this->favorites->where( 'user_id', auth()->id() )->count();
     }
 
 }
