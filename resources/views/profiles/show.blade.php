f6b22dd449ba4f8a79f14e719f6a0a898c6d30ac
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-18 12:35:51 +0100

    profiles.show added threads to template
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -6,5 +6,18 @@
             {{ $profileUser->name }}
             <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
         </div>
+
+        @foreach ( $profileUser->threads as $thread )
+            <div class="card">
+                <div class="card-header">
+                    <a href="#">{{ $thread->creator->name }}</a> posted:
+                    {{ $thread->title }}
+                </div>
+
+                <div class="card-body">
+                    {{ $thread->body }}
+                </div>
+            </div>
+        @endforeach
     </div>
 @endsection
