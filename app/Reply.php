2615882884e47bfa706bd2778c9eaf750bf06038
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-18 11:17:00 +0100

    Move favorites related methods into separate trait
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Reply.php
+++ app/Reply.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    use Favoritable;
+
     protected $guarded = [];
 
     protected $with = [ 'owner', 'favorites' ];
@@ -17,27 +19,4 @@ class Reply extends Model
         return $this->belongsTo( User::class, 'user_id' );
     }
 
-    public function favorites()
-    {
-        return $this->morphMany( Favorite::class, 'favorited' );
-    }
-
-    public function favorite( $userId )
-    {
-        $attributes = [ 'user_id' => $userId ];
-
-        if (! $this->favorites()->where( $attributes )->exists() ) {
-            $this->favorites()->create( $attributes );
-        }
-    }
-
-    public function isFavorited()
-    {
-        return !!$this->favorites->where( 'user_id', auth()->id() )->count();
-    }
-
-    public function getFavoritesCountAttribute()
-    {
-        return $this->favorites->count();
-    }
 }
