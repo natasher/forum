e3bb77b1b9b1c0e59a9d0eabfce8718e9f1b5226
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-18 10:35:27 +0100

    Added custom getter favoritesCount to Reply
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Reply.php
+++ app/Reply.php
@@ -36,4 +36,8 @@ class Reply extends Model
         return !!$this->favorites->where( 'user_id', auth()->id() )->count();
     }
 
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
+    }
 }
