d86430729adf737a6dfb798429a9f4d41dfa65c2
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-18 11:29:44 +0100

    Added TC that fails
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- tests/Feature/ProfilesTest.php
+++ tests/Feature/ProfilesTest.php
@@ -2,11 +2,21 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProfilesTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    public function a_user_has_a_profile()
+    {
+        $user = create( User::class );
+
+        $this->get( "/profiles/{{ $user->name }}" )
+            ->assertSee( $user->name );
+    }
 
 }
