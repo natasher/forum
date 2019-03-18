459916a1e871d5c488d56838f63f7129a66d94f7
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-18 14:28:22 +0100

    Added link to show My Profile
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -60,6 +60,8 @@
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
+                            <a href="{{ route( 'profile', Auth::user() ) }}" class="dropdown-item">My Profile</a>
+
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
