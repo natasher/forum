ef578323a8ada9f4906f23aa15d20d692103bae0
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-18 11:55:19 +0100

    refactor profiles.show template
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,5 +1,10 @@
 @extends( 'layouts.app' )
 
 @section( 'content' )
-    {{ $profileUser->name }}
+    <div class="container">
+        <div class="pb-2 mt-4 mb-2 border-bottom display-3">
+            {{ $profileUser->name }}
+            <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
+        </div>
+    </div>
 @endsection
