542bb4cc192c6268ce271bd8ccc053ea1d187352
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-20 11:56:11 +0100

    Created Activities Test
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

---
+++ tests/Unit/ActivitTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Thread;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ActivityTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function it_records_activity_when_a_thread_is_created()
+    {
+        $this->signIn();
+
+        $thread = create( Thread::class );
+
+        $this->assertDatabaseHas( 'activities', [
+            'type'         => 'created_thread',
+            'user_id'      => auth()->id(),
+            'subject_id'   => $thread->id,
+            'subject_type' => Thread::class,
+        ]);
+    }
+}
