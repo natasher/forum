b003bfc8f72fddaf32bb441f1d904b736d8b7826
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-20 12:28:07 +0100

    Filled out activity migration schema
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- database/migrations/2019_03_20_103410_create_activities_table.php
+++ database/migrations/2019_03_20_103410_create_activities_table.php
@@ -15,6 +15,10 @@ class CreateActivitiesTable extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger( 'user_id' )->index();
+            $table->string( 'type', 50 );
+            $table->unsignedInteger( 'subject_id' )->index();
+            $table->string( 'subject_type', 50 );
             $table->timestamps();
         });
     }
