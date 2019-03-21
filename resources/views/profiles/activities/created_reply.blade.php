c6f3f085517ebbd34e97a770300b08028737f8b1
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-21 16:35:42 +0100

    refactor
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/views/profiles/activities/created_reply.blade.php
+++ resources/views/profiles/activities/created_reply.blade.php
@@ -1,1 +1,13 @@
-{{ $profileUser->name }} replied to thread
+<div class="card mt-2">
+    <div class="card-header">
+        <div class="level">
+            <span class="flex">
+                {{ $profileUser->name }} replied to thread
+            </span>
+        </div>
+    </div>
+
+    <div class="card-body">
+        {{ $activity->subject->body }}
+    </div>
+</div>
