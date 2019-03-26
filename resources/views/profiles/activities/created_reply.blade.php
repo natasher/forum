36df49ed7b369e36c7ad42cf92927d1ca49481ff
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-26 15:21:07 +0100

    Refactor created_reply to use component
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/views/profiles/activities/created_reply.blade.php
+++ resources/views/profiles/activities/created_reply.blade.php
@@ -1,14 +1,12 @@
-<div class="card mt-2">
-    <div class="card-header">
-        <div class="level">
-            <span class="flex">
-                {{ $profileUser->name }} replied to
-                <a href="{{ $activity->subject->thread->path() }}">"{{ $activity->subject->thread->title }}"</a>
-            </span>
-        </div>
-    </div>
+@component( 'profiles.activities.activity' )
 
-    <div class="card-body">
+    @slot( 'heading' )
+        {{ $profileUser->name }} replied to
+        <a href="{{ $activity->subject->thread->path() }}">"{{ $activity->subject->thread->title }}"</a>
+    @endslot
+
+    @slot( 'body' )
         {{ $activity->subject->body }}
-    </div>
-</div>
+    @endslot
+
+@endcomponent
