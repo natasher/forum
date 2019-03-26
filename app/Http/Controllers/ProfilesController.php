428aecf449e601309d448ff9aa403b6aba986e3a
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-26 16:39:58 +0100

    Refactor ProfilesCtrl#show, extract query to separate method
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,14 +10,19 @@ class ProfilesController extends Controller
 
     public function show(User $user)
     {
-        $activities = $user->activity()->latest()->with('subject')->get()->groupBy(function ( $activity ) {
-            return $activity->created_at->format( 'Y-m-d');
-        });
+        $activities = $this->getActivity( $user );
 
         return view( 'profiles.show', [
             'profileUser' => $user,
             'activities'  => $activities,
         ]);
+    }
+
+    protected function getActivity( User $user )
+    {
+        return $user->activity()->latest()->with('subject')->take( 50 )->get()->groupBy(function ( $activity ) {
+            return $activity->created_at->format( 'Y-m-d');
+        });
     }
 
 }
