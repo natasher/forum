3c97cd22ac53a8ef18e8276b3c777925c6821abc
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-26 16:40:31 +0100

    Inline
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,11 +10,9 @@ class ProfilesController extends Controller
 
     public function show(User $user)
     {
-        $activities = $this->getActivity( $user );
-
         return view( 'profiles.show', [
             'profileUser' => $user,
-            'activities'  => $activities,
+            'activities'  => $this->getActivity( $user ),
         ]);
     }
 
