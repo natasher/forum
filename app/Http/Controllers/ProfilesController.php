1c16378937a251aa9ed71ed41cc7d5d5c4f19fd0
Artur Woźniak <artur.wozniak69@gmail.com>
2019-03-27 10:01:05 +0100

    design feed static method on activity model
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Activity;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
@@ -12,7 +13,7 @@ class ProfilesController extends Controller
     {
         return view( 'profiles.show', [
             'profileUser' => $user,
-            'activities'  => $this->getActivity( $user ),
+            'activities'  => Activity::feed( $user ),
         ]);
     }
 
