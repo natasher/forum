3ddeff26ae0cd18bc9f963f0f9b75d4caf9d0788
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-01 15:42:23 +0200

    smooth refactor
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- tests/Feature/FavoritesTest.php
+++ tests/Feature/FavoritesTest.php
@@ -41,10 +41,10 @@ class FavoritesTest extends TestCase
 
         $reply = create( Reply::class );
 
-        $this->post( 'replies/' . $reply->id . '/favorites' );
-        $this->assertCount( 1, $reply->favorites );
+        $reply->favorite();
 
         $this->delete( 'replies/' . $reply->id . '/favorites' );
+
         $this->assertCount( 0, $reply->fresh()->favorites );
     }
 
