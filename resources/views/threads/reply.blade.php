25d76507a9b40c012550469059777a0443b57cb4
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-01 12:12:18 +0200

    Wrap reply.blade with <reply inline-template />
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,38 +1,40 @@
-<div id="reply-{{ $reply->id }}" class="card mt-2">
-    <div class="card-header">
-        <div class="level">
-            <h5 class="flex">
-                <a href="{{ route( 'profile' , $reply->owner ) }}">
-                    {{ $reply->owner->name }}
-                </a> said {{ $reply->created_at->diffForHumans() }}...
-            </h5>
+<reply inline-template>
+    <div id="reply-{{ $reply->id }}" class="card mt-2">
+        <div class="card-header">
+            <div class="level">
+                <h5 class="flex">
+                    <a href="{{ route( 'profile' , $reply->owner ) }}">
+                        {{ $reply->owner->name }}
+                    </a> said {{ $reply->created_at->diffForHumans() }}...
+                </h5>
 
-            <div>
-                <form action="/replies/{{ $reply->id }}/favorites" method="post">
-                    @csrf
-                    <button type="submit" class="btn" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                        {{ $reply->favorites_count }} {{ str_plural( 'Favorite', $reply->favorites_count ) }}
-                    </button>
-                </form>
+                <div>
+                    <form action="/replies/{{ $reply->id }}/favorites" method="post">
+                        @csrf
+                        <button type="submit" class="btn" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                            {{ $reply->favorites_count }} {{ str_plural( 'Favorite', $reply->favorites_count ) }}
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="card-body">
-        {{ $reply->body }}
-    </div>
-
-    @can ( 'update', $reply )
-        <div class="card-footer level">
-            <button class="btn btn-xs mr-1">Edit</button>
-
-            <form action="/replies/{{ $reply->id }}" method="post">
-                @csrf
-                @method( 'DELETE' )
-
-                <button type="submit" class="btn btn-danger btn-xs">Delete</button>
-            </form>
+        <div class="card-body">
+            {{ $reply->body }}
         </div>
-    @endcan
 
-</div>
+        @can ( 'update', $reply )
+            <div class="card-footer level">
+                <button class="btn btn-xs mr-1">Edit</button>
+
+                <form action="/replies/{{ $reply->id }}" method="post">
+                    @csrf
+                    @method( 'DELETE' )
+
+                    <button type="submit" class="btn btn-danger btn-xs">Delete</button>
+                </form>
+            </div>
+        @endcan
+
+    </div>
+</reply>
