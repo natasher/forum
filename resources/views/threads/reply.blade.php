438ab067f7cf1ed1cbccd2c6a3880e4588e6b98d
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-01 11:14:46 +0200

    Added delete btn to reply template
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -21,4 +21,16 @@
     <div class="card-body">
         {{ $reply->body }}
     </div>
+
+    @can ( 'update', $reply )
+        <div class="card-footer">
+            <form action="/replies/{{ $reply->id }}" method="post">
+                @csrf
+                @method( 'DELETE' )
+
+                <button type="submit" class="btn btn-danger btn-xs">Delete</button>
+            </form>
+        </div>
+    @endcan
+
 </div>
