8f85655cf5a3bf84bccf3163de8bb653f1a3e726
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-05 11:15:18 +0200

    Move Reply created_at from carbon to moment
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/js/components/ReplyComponent.vue
+++ resources/js/components/ReplyComponent.vue
@@ -5,7 +5,7 @@
                 <h5 class="flex">
                     <a :href="`/profiles/${ data.owner.name }`"
                         v-text="data.owner.name">
-                    </a> said {{ data.created_at }}...
+                    </a> said <span v-text="ago"></span>...
                 </h5>
 
                 <div v-if="signedIn">
@@ -54,6 +54,7 @@
 
 <script>
     import favorite from './FavoriteComponent.vue'
+    import * as moment from 'moment';
 
     export default {
 
@@ -78,6 +79,10 @@
 
             canUpdate: function () {
                 return this.authorize( user => this.data.user_id == user.id );
+            },
+
+            ago() {
+                return moment( this.data.created_at ).fromNow();
             },
         },
 
