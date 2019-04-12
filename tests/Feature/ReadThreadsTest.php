b6bddd38c9c57b224027865c60a4d953dec6e292
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-12 10:30:03 +0200

    hydrate TC, RED
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -81,7 +81,12 @@ class ReadThreadsTest extends TestCase
     /** @test */
     function a_user_can_filter_threads_by_those_that_are_unanswered()
     {
+        $thread = create( Thread::class );
+        create( Reply::class, [ 'thread_id' => $thread->id ]);
 
+        $response = $this->getJson( 'threads?unanswered=1' )->json();
+
+        $this->assertCount( 1, $response );
     }
 
     /** @test */
