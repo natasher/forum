c88e8fc893b68bc0e582a27fcb854b90c5c3d75d
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-15 16:58:40 +0200

    Added new TC bout unsubscribe
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- tests/Feature/SubscribeToThreadsTest.php
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -35,4 +35,16 @@ class SubscribeToThreadsTest extends TestCase
         // $this->assertCount( 1, auth()->user()->notifications );
     }
 
+    /** @test */
+    public function a_user_can_unsubscribe_from_threads()
+    {
+        $this->signIn();
+
+        $thread = create( Thread::class );
+
+        $this->delete( $thread->path() . '/subscriptions' );
+
+        $this->assertCount( 0, $thread->subscriptions );
+    }
+
 }
