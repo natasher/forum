a6aa74b62885c2612744b91fb059ec1f04940de4
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-15 16:05:45 +0200

    Rename TS
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- tests/Feature/SubscribeToThreadsTest.php
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ThreadTest extends TestCase
+class SubscribeToThreadsTest extends TestCase
 {
 
     use RefreshDatabase;
