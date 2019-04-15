8ea5ca60a51c40e8770a84b60f37285023320db3
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-15 13:41:20 +0200

    Added new Feature Test
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

---
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use App\Thread;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ThreadTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_subscribe_to_threads()
+    {
+
+    }
+
+}
