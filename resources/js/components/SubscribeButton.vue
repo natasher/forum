52d78131b08a629e6342f91bd69aa454858dd80a
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-15 14:58:19 +0200

    Added on:click on subscribe button
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/js/components/SubscribeButton.vue
+++ resources/js/components/SubscribeButton.vue
@@ -1,11 +1,27 @@
 <template>
-    <button class="btn btn-default">Subscribe</button>
+    <button
+        class="btn btn-default"
+        @click="subscribe">
+            Subscribe
+    </button>
 </template>
 
 <script>
 export default {
 
     name: 'SubscribeButton',
+
+    methods: {
+        subscribe() {
+            axios.post( `${ location.pathname }/subscriptions` )
+                .then(function () {
+                    flash( 'Subscribed' )
+                })
+                .catch(function ( e ) {
+                    console.error( 'something fucks up' )
+                })
+        },
+    },
 
 }
 </script>
