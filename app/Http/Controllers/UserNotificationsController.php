8e142a37480a106697b9e30a1951621882df4baf
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-16 13:19:52 +0200

    create UserNotificatinsCtrl#index
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Http/Controllers/UserNotificationsController.php
+++ app/Http/Controllers/UserNotificationsController.php
@@ -13,6 +13,11 @@ class UserNotificationsController extends Controller
         $this->middleware( 'auth' );
     }
 
+    public function index()
+    {
+        return auth()->user()->unreadNotifications;
+    }
+
     public function destroy( User $user, $notificationId )
     {
         auth()->user()->notifications()->findOrFail( $notificationId )->markAsRead();
