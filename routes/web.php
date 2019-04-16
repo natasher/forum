52354e9df791c8afbe7a471bf64a4eaaf6e1ba26
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-16 12:54:18 +0200

    Added new route
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,4 @@ Route::post( '/replies/{reply}/favorites', 'FavoritesController@store' );
 Route::delete( '/replies/{reply}/favorites', 'FavoritesController@destroy' );
 
 Route::get( '/profiles/{user}', 'ProfilesController@show' )->name( 'profile' );
+Route::delete( '/profiles/{user}/notifications', 'UserNotifications@destory' );
