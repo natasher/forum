bd24021e6773f9e9be2889f7731678cf33054dd9
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-16 15:26:33 +0200

    fill template with static content
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- resources/js/components/UserNotifications.vue
+++ resources/js/components/UserNotifications.vue
@@ -1,4 +1,17 @@
 <template>
+    <div class="dropdown">
+
+        <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Notifications
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+
+    </div>
 </template>
 
 <script>
