256b470de18e2891f97b0dd9b2a605e95a29d72c
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-17 12:38:55 +0200

    Clean up ThreadHasNewReply event
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Events/ThreadHasNewReply.php
+++ app/Events/ThreadHasNewReply.php
@@ -2,17 +2,11 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class ThreadHasNewReply
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use SerializesModels;
 
     /**
      * Create a new event instance.
@@ -24,13 +18,4 @@ class ThreadHasNewReply
         //
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
 }
