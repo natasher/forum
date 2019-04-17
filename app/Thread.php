a337df47bb7ba02e728cfa7ab605f78e16aab2dc
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-17 12:34:02 +0200

    Use events to handle addReply
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Thread.php
+++ app/Thread.php
@@ -54,10 +54,7 @@ class Thread extends Model
     {
         $reply = $this->replies()->create( $reply );
 
-        $this->subscriptions
-            ->where( 'user_id', '!=', $reply->user_id )
-            ->each
-            ->notify( $reply );
+        event( new ThreadHasNewReply );
 
         return $reply;
     }
