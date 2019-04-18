2a9cabca867be8b918ee1bd2ff61ed63575a7fb1
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-18 14:51:42 +0200

    figure out how our class should work
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Spam.php
+++ app/Spam.php
@@ -5,9 +5,24 @@ namespace App;
 class Spam
 {
 
-    public function detect()
+    public function detect( $body )
     {
+        $this->detectInvalidKeywords( $body );
 
+        return false;
+    }
+
+    protected function detectInvalidKeywords( $body )
+    {
+        $invalidKeyword = [
+            'yahoo customer support',
+        ];
+
+        foreach( $invalidKeyword as $keyword ) {
+            if ( stripos( $body, $keyword ) !== false ) {
+                throw new \Exception( 'Your reply contains spam' );
+            }
+        }
     }
 
 }
