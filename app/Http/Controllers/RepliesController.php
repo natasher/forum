61e853fff1e802c439db433027311d7ceb417ee6
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-24 13:41:43 +0200

    Refactor RepliesCtrl#store to send 422 due to error
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -42,18 +42,21 @@ class RepliesController extends Controller
      */
     public function store($channelId, Thread $thread)
     {
-        $this->validateReply();
+        try {
+            $this->validateReply();
 
-        $reply = $thread->addReply([
-            'body'    => request( 'body' ),
-            'user_id' => auth()->id()
-        ]);
-
-        if ( request()->expectsJson() ) {
-            return $reply->load( 'owner' );
+            $reply = $thread->addReply([
+                'body'    => request( 'body' ),
+                'user_id' => auth()->id()
+            ]);
+        } catch ( \Exception $e ) {
+            return response(
+                'Sorry, your reply could not be saved at this time.',
+                422
+            );
         }
 
-        return back()->with( 'flash', 'Your reply has been left.' );
+        return $reply->load( 'owner' );
     }
 
     /**
