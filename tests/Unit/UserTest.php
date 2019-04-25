7ddc34de6487d8ed4046096e846b344f68f736cb
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-25 12:18:42 +0200

    Fill in TC `user can fetch last reply`
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace Test\Feature;
+
+use App\User;
+use App\Reply;
+use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
@@ -10,7 +14,11 @@ class UserTest extends TestCase
     /** @test */
     function a_user_can_fetch_their_most_recent_reply()
     {
+        $user = create( User::class );
 
+        $reply = create( Reply::class, [ 'user_id' => $user->id ]);
+
+        $this->assertEquals( $reply->id, $user->lastReply->id );
     }
 
 }
