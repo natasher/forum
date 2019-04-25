88eeba7c167ffe3c2797eeb6157adb24af078550
Artur Woźniak <artur.wozniak69@gmail.com>
2019-04-25 12:15:16 +0200

    Added UserTest unit TS
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test\Feature;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function a_user_can_fetch_their_most_recent_reply()
+    {
+
+    }
+
+}
